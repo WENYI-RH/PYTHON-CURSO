72519e6e3a1f5a3f9e10bc66dac0a6e46cf356d0
WENYI-RH <wenyi.ruelas@ucsp.edu.pe>
2025-01-11 04:31:21 +0000

    Practica numero 1 05/01

---
+++ Practica1py/Ejercicio4.py
@@ -0,0 +1,3 @@
+numero = int(input("Ingrese un número entero: "))
+suma = numero * (numero + 1) // 2 
+print("La suma de los números de 1 a " + str(numero) + " es: " + str(suma))
---
+++ Practica1py/Ejericicio3.py
@@ -0,0 +1,6 @@
+#Verificar numero 
+numero = int(input("Ingrese un número entero: "))
+if (numero // 2) * 2 == numero:
+    print("El número es par.")
+else:
+    print("El número es impar.")
