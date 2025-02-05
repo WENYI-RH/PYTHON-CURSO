5b3ee30dae15a27f9c33ccc9d4ab2e963b8ea713
WENYI-RH <wenyi.ruelas@ucsp.edu.pe>
2025-02-06 04:04:37 +1200

    Delete Repaso.py directory

--- Repaso.py/repaso1.py
+++
@@ -1,8 +0,0 @@
-a = float(input("ingresar numero 1: "))
-b = float(input("ingresar numero 2: "))
-
-igualdad = a == b
-mayor = a > b
-mayor2 = b > a
-
-print(f"los numeros son iguales {igualdad}, el numero 1 es mayor que el 2 {mayor}")
